cf2599b80db785a2281fee1fa67540314f0610b9
yzbbanban <yzbbanban@live.com>
2016-08-05 23:31:00 +0800

    语音视频测试

---
+++ app/src/main/java/com/wangban/yzbbanban/test_wangyiyunxin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wangban.yzbbanban.test_wangyiyunxin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/wangban/yzbbanban/test_wangyiyunxin/NimApplication.java
@@ -0,0 +1,15 @@
+package com.wangban.yzbbanban.test_wangyiyunxin;
+
+import android.app.*;
+
+/**
+ * Created by YZBbanban on 16/8/5.
+ */
+
+public class NimApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
